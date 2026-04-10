dabe44eb6a37f9c92b681a410edd3fc4b06b1965
Tragiang118 <2251172322@e.tlu.edu.vn>
2026-04-11 00:47:11 +0700

    implement categories module with controller and service

---
+++ Backend/src/categories/categories.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class CategoriesService {
+  constructor(private prisma: PrismaService) {}
+
+  findAll() {
+    return this.prisma.category.findMany();
+  }
+
+  findOne(id: number) {
+    return this.prisma.category.findUnique({
+      where: { id },
+    });
+  }
+}
